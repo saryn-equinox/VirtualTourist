a4850e5536a2fc78b03e963c3395ac6a660ea06f
Haoting Qiu <easonyau@126.com>
2021-01-07 01:37:38 -0500

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by 邱浩庭 on 7/1/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
